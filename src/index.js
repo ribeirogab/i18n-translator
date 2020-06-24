9addc7bc4a8969b0af83e99691fa77d141f800ed
Gabriel Ribeiro <gabriel@Gabriels-MacBook-Air.local>
2020-06-24 14:37:38 -0300

    fix: remove files and tidy functionality

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 const API_KEY = require('./key/credential.json').API_KEY;
-const input = require('./content/input');
 
 const translate = new Translate({
   key: API_KEY,
@@ -12,20 +11,26 @@ const translate = new Translate({
 async function start() {
   const outputLanguages = ['en', 'es', 'pt'];
 
-  outputLanguages.forEach(language => {
-    let output = '';
+  const data = fs.readFileSync('./input.txt', 'utf-8');
+  const lines = data.split('\n');
 
-    Object.keys(input).forEach(async (item, index) => {
-      const [translation] = await translate.translate(input[item], language);
-      output += `${item}: ${translation}\n`;
+  for (const language of outputLanguages) {
+    const translatedLines = [];
 
-      if (Object.keys(input).length - 1 === index) {
-        fs.writeFileSync(
-          path.join(__dirname, `./content/output/${language}.txt`), output, 'utf8'
-        );
+    for (const line of lines) {
+      if (line.length === 0 || line[0] === '/')  {
+        translatedLines.push(line);
+        continue;
       }
-    });
-  });
+
+      const phrase = line.split(": '")[1].replace("',", '');
+      const [translation] = await translate.translate(phrase, language);
+      const newLine = `${line.split(": '")[0]}: "${translation}",`
+      translatedLines.push(newLine);
+    }
+
+    fs.writeFileSync(`./output/${language}.txt`, translatedLines.join('\n'), 'utf-8');
+  }
 }
 
 start();
